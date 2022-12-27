52b58c1f5428dad360de1372d873e830771f0708
abdelhadicodi <aboude.arab12@gmail.com>
2022-12-27 16:14:02 +0200

    step-5

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الارهاب و الكباب', year: 1992, rating: 6.2 }
+]
 app.get('/:test1?/:test2?', (req,res) =>{
     let test1 = req.params.test1;
     let test2 = req.params.test2;
@@ -37,6 +44,17 @@ else if (test1 == "search"){
         let search = { status:200 , message: "ok"};
         res.send(search)
     }
+}
+else if (test1 == "movies"){
+     test2 == "create"
+    ? res.send("create movie")
+    : test2 == "read"
+    ? res.send ({status:200, data:movies})
+    : test2 == "update"
+    ? res.send ("update movie")
+    : test2 == "delete"
+    ? res.send("delete movie")
+    : res.send("choose what you need from movies (create, read, update, delete)")
 }
 })
 
