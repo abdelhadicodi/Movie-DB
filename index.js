4f50d189503ade06e1d158f89afecbbcf124037a
abdelhadicodi <aboude.arab12@gmail.com>
2022-12-28 20:00:14 +0200

    step-6

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const movies = [
     { title: 'Brazil', year: 1985, rating: 8 },
     { title: 'الارهاب و الكباب', year: 1992, rating: 6.2 }
 ]
-app.get('/:test1?/:test2?', (req,res) =>{
+app.get('/:test1?/:test2?/:test3?', (req,res) =>{
     let test1 = req.params.test1;
     let test2 = req.params.test2;
+    let test3 =req.params.test3;
 
 if (test1 == undefined){
     res.send("ok");
@@ -46,19 +47,41 @@ else if (test1 == "search"){
     }
 }
 if (test1 == "movies" && test2 == "create"){
-      res.send("create movie")
- }
-else if (test1 == "movies" && test2 == "read"){
-      res.send ({status:200, data:movies})
- }
-else if (test1 == "movies" && test2 == "update"){
-     res.send ("update movie")
- }
-else if (test1 == "movies" && test2 == "delete"){
-     res.send("delete movie")}
-else if (test1 == "movies" && test2 == undefined){
-    res.send("choose what you need from movies (create, read, update, delete)")
-}
+      res.send("create movie")}
+        else if (test1 == "movies" && test2 == "update"){
+          res.send ("update movie")}
+        else if (test1 == "movies" && test2 == "delete"){
+          res.send("delete movie")}
+        else if (test1 == "movies" && test2 == undefined){
+          res.send("choose what you need from movies (create, read, update, delete)")}  
+
+if (test1 == "movies" && test2 == "read"){
+        if (test3 == undefined){
+            res.send({status:200, data:movies})}
+        else if (test3 == "by-date"){
+            var movieorder
+            movieorder = movies.sort((a,b)=>{
+                return a.year - b.year;
+            });
+            }
+        else if (test3 == "by-rating"){
+            movieorder = movies.sort((a, b) =>{
+                return b.rating - a.rating
+            });
+            }
+        else if (test3 == "by-title"){
+            movieorder = movies.sort((a, b) =>{
+                if (a.title <  b.title){
+                    return -1;
+                    }
+                if (a.title > b.title){
+                    return 1;
+                    }
+                    return 0;
+                })
+            } 
+    }
+
 })
 
 app.listen(port, () => {
